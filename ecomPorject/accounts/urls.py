2bed8469f8cad7729fe189d2cf924c95936f54c4
Mudassar Amin <mudasiramin456@gmail.com>
2023-06-04 22:01:00 +0500

    edit more

--- ecomPorject/accounts/urls.py
+++ ecomPorject/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# sign up login url 
+# sign up login url pattren
 urlpatterns = [
     path('userSignUp/', views.userSignUp, name='userSignUp'),
     path('user_login/', views.user_login, name='user_login'),
